fdd8259a53123bda6bee4f99252bae38830c4d99
Igor Korchagin <igor.korchagin8@gmail.com>
2022-11-01 20:03:53 +0100

    add lifecycle handlers

--- src/metric-reporter.module.ts
+++ src/metric-reporter.module.ts
@@ -1,28 +1,18 @@
-import {
-  Inject,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  OnApplicationBootstrap,
-  OnApplicationShutdown,
-} from '@nestjs/common';
+import { Inject, MiddlewareConsumer, Module, NestModule, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { MetricReporterMiddleware } from './metric-reporter.middleware';
 import { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE } from './metric-reporter.module-definition';
 
 @Module({})
-export class MetricReporterModule
-  extends ConfigurableModuleClass
-  implements OnApplicationBootstrap, OnApplicationShutdown, NestModule
-{
+export class MetricReporterModule extends ConfigurableModuleClass implements OnModuleInit, OnModuleDestroy, NestModule {
   constructor(@Inject(MODULE_OPTIONS_TOKEN) protected readonly options: typeof OPTIONS_TYPE) {
     super();
   }
 
-  public async onApplicationBootstrap() {
+  public async onModuleInit() {
     this.options.reporter.start();
   }
 
-  public async onApplicationShutdown() {
+  public async onModuleDestroy() {
     this.options.reporter.stop();
     await this.options.reporter.flush();
   }
