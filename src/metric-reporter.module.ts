73cf7d71993bc7cc0a345957a8dca86f813c1346
Igor Korchagin <igor.korchagin8@gmail.com>
2022-11-01 08:49:54 +0100

    flush metrics on application shutdown

--- src/metric-reporter.module.ts
+++ src/metric-reporter.module.ts
@@ -1,22 +1,33 @@
-import { Inject, MiddlewareConsumer, Module, NestModule, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Inject,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
 import { MetricReporterMiddleware } from './metric-reporter.middleware';
 import { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE } from './metric-reporter.module-definition';
 
 @Module({})
-export class MetricReporterModule extends ConfigurableModuleClass implements OnModuleDestroy, OnModuleInit, NestModule {
+export class MetricReporterModule
+  extends ConfigurableModuleClass
+  implements OnApplicationBootstrap, OnApplicationShutdown, NestModule
+{
   constructor(@Inject(MODULE_OPTIONS_TOKEN) protected readonly options: typeof OPTIONS_TYPE) {
     super();
   }
 
-  onModuleInit() {
+  public async onApplicationBootstrap() {
     this.options.reporter.start();
   }
 
-  onModuleDestroy() {
+  public async onApplicationShutdown() {
     this.options.reporter.stop();
+    await this.options.reporter.flush();
   }
 
-  configure(consumer: MiddlewareConsumer) {
+  public configure(consumer: MiddlewareConsumer) {
     const config = consumer.apply(MetricReporterMiddleware);
     if (this.options.excludedRoutes) config.exclude(...this.options.excludedRoutes);
     config.forRoutes(...this.options.routes);
